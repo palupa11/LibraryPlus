449e32e89d669b29da777809ca9036085f298726
palupa11 <paulina.luisce@gmail.com>
2025-05-03 15:50:45 +1000

    Added LinkedList and Node class

---
+++ LibraryPlusAssignment/LibraryPlusAssignment/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryPlusAssignment
+{
+    internal class Node<T>
+    {
+        private T data;   //Makes it generic to allow any type of data (member is an object?)
+        private Node<T> next; //Storing a reference to another node object, which is the next node in the list
+
+        //Properties 
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public Node<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+    }
+}
