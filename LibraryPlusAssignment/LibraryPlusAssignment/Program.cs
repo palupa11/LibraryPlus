ec28be98354fe1f31d64b72fbbc545aa8fbbad1b
palupa11 <paulina.luisce@gmail.com>
2025-05-25 12:12:35 +1000

    Changes for testing in main function

--- LibraryPlusAssignment/LibraryPlusAssignment/Program.cs
+++ LibraryPlusAssignment/LibraryPlusAssignment/Program.cs
@@ -94,12 +94,20 @@ namespace LibraryPlusAssignment
         {
             Console.WriteLine("=========Movie Title Generator TEST ==========");
             ExperimentalTests.GenerateTestMovies(2000);
-            //int[] testTitles = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000];
-            //for (int i = 0; i < testTitles.Length; i++)
-            //{ 
-            //    ExperimentalTests.GenerateTestMovies(testTitles[i]);
+            Console.WriteLine("=========Running DisplayTopThree() Test ==========");
+            MemberCollection memberCollection = MemberCollection.GetInstance();
+            Member? testMember = memberCollection.MemberLogIn("ti", "to", "1000");
 
-            //}
+            if (testMember != null)
+            {
+                MovieCollection movieCollection = MovieCollection.GetInstance();
+                testMember.DisplayTopThree(movieCollection);
+            }
+            else
+            {
+                Console.WriteLine("Test member not found.");
+            }
+
 
 
             Console.WriteLine("===================================================================================");
